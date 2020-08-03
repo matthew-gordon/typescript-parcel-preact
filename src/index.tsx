14e19f84ef893e95afcb661634861714d99b5b10
matthew-gordon <m.gordon5525@gmail.com>
2020-08-03 16:49:07 -0600

    Prettier working

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-import { h, render } from "preact";
+import { h, render } from 'preact'
 
 interface AppProps {
-  name: string;
+  name: string
 }
 
-const App = ({ name }: AppProps) => <h1>Hello from {name}!</h1>;
+const App = ({ name }: AppProps) => <h1>Hello from {name}!</h1>
 
-render(
-  <App name={"Jeff's typescript preact starter"} />,
-  document.getElementById("root")
-);
+render(<App name={"Jeff's typescript preact starter"} />, document.getElementById('root'))
