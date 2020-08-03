53365e0e1e3a5a02aa6b8b6eadd52a3f1760f3ca
matthew-gordon <m.gordon5525@gmail.com>
2020-08-03 16:43:51 -0600

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { h, render } from "preact";
+
+interface AppProps {
+  name: string;
+}
+
+const App = ({ name }: AppProps) => <h1>Hello from {name}!</h1>;
+
+render(
+  <App name={"Jeff's typescript preact starter"} />,
+  document.getElementById("root")
+);
